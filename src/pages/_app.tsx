cf6e199c38de7cbdb73ef6d5a7aaa8f748929aa6
ATwinn <dinhducluong110@gmail.com>
2023-03-09 20:18:01 +0700

    setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
+import 'antd/dist/reset.css';
+import '../styles/globals.css'
+import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import store from '../redux/store'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient()
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
 }
+
+export default MyApp
---
+++ src/redux/store.ts
@@ -0,0 +1,24 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux'
+
+// Import các reducers của bạn
+import counterReducer from './counterSlice'
+
+// Tạo store với reducer của bạn
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+})
+
+// Export các kiểu cho useSelector hook
+export type RootState = ReturnType<typeof store.getState>
+
+// Export hook useSelector với kiểu RootState
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+// Export useDispatch hook để dispatch các action
+export type AppDispatch = typeof store.dispatch
+export const useAppDispatch = () => useDispatch()
+
+export default store
