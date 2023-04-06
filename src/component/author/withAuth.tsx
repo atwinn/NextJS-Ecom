c4aec1072e524bcf7c1b5090b27bb00a353e62be
ATwinn <dinhducluong110@gmail.com>
2023-04-07 00:11:56 +0700

    fix build

--- src/component/author/withAuth.tsx
+++ src/component/author/withAuth.tsx
@@ -1,28 +1,28 @@
-// import React from 'react';
-// import { NextPage } from 'next';
-// import { useRouter } from "next/router";
+import React from 'react';
+import { NextPage } from 'next';
+import { useRouter } from "next/router";
 
-// const withAuth = <P extends object>(Component: NextPage<P>): NextPage<P> => {
-//     const Auth: NextPage<P> = ({ ...props }) => {
-//         // If user is not logged in, redirect to login page
-//         const userAuth = typeof window !== 'undefined' ? localStorage.getItem("username") : null;
+const withAuth = <P extends object>(Component: NextPage<P>): NextPage<P> => {
+    const Auth: NextPage<P> = ({ ...props }) => {
+        // If user is not logged in, redirect to login page
+        const userAuth = typeof window !== 'undefined' ? localStorage.getItem("username") : null;
 
-//         const router = useRouter();
+        const router = useRouter();
 
-//         if (!userAuth && typeof window !== 'undefined') {
-//             // Redirect to login page
-//             router.push('/auth/login');
-//             return null;
-//         }
-//         return <Component {...props as P} />;
-//     };
+        if (!userAuth && typeof window !== 'undefined') {
+            // Redirect to login page
+            router.push('/auth/login');
+            return null;
+        }
+        return <Component {...props as P} />;
+    };
 
-//     // Copy getInitialProps so it will run as well
-//     if (Component.getInitialProps) {
-//         Auth.getInitialProps = Component.getInitialProps;
-//     }
+    // Copy getInitialProps so it will run as well
+    if (Component.getInitialProps) {
+        Auth.getInitialProps = Component.getInitialProps;
+    }
 
-//     return Auth;
-// };
+    return Auth;
+};
 
-// export default withAuth;
+export default withAuth;
