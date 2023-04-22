f8ec0690aa9ac787bbf4d40a1d1ee2dd48d7f0ff
manhdo1 <dodinhmanh75@gmail.com>
2023-04-22 21:51:23 +0700

    thong ke process...90%

--- src/component/charts/chart.tsx
+++ src/component/charts/chart.tsx
@@ -1,72 +1,62 @@
-import React from "react";
-const Column = dynamic(() => import('@ant-design/plots').then(({ Column }) => Column), { ssr: false });
+import React, { useEffect, useState } from "react";
+const Column = dynamic(
+  () => import("@ant-design/plots").then(({ Column }) => Column),
+  { ssr: false }
+);
 import { Card } from "antd";
 import dynamic from "next/dynamic";
+import axios from "axios";
 
 const DemoColumn = () => {
-  const data = [
-    {
-      type: "家具家电",
-      sales: 38,
-    },
-    {
-      type: "粮油副食",
-      sales: 52,
-    },
-    {
-      type: "生鲜水果",
-      sales: 61,
-    },
-    {
-      type: "美容洗护",
-      sales: 145,
-    },
-    {
-      type: "母婴用品",
-      sales: 48,
-    },
-    {
-      type: "进口食品",
-      sales: 38,
-    },
-    {
-      type: "食品饮料",
-      sales: 38,
-    },
-    {
-      type: "家庭清洁",
-      sales: 38,
-    },
-  ];
-  const config: any = {
+  const [dataCol, setDataCol] = useState([]);
+  useEffect(() => {
+    axios
+      .get("/api/thongKe12ThangGanNhat")
+      .then((res) => {
+        // console.log(res.data.result);
+        setDataCol(res.data.result)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  let data:any = [];
+  dataCol.forEach((item:any) => {
+    data.push({
+      thang: item.month,
+      type: "Tổng Bán",
+      value: item.total_ban,
+    });
+  
+    data.push({
+      thang: item.month,
+      type: "Doanh thu (triệu đồng)",
+      value: (item.total_doanhThu / 1000000).toFixed(2),
+    });
+  
+    data.push({
+      thang: item.month,
+      type: "Chi tiêu (triệu đồng)",
+      value: (item.total_chiTieu / 1000000).toFixed(2) ,
+    });
+  
+    data.push({
+      thang: item.month,
+      type: "Tổng phiếu nhập",
+      value: item.total_nhap,
+    });
+  });
+  const config = {
     data,
-    xField: "type",
-    yField: "sales",
-    label: {
-      // 可手动配置 label 数据标签位置
-      position: "middle",
-      // 'top', 'bottom', 'middle',
-      // 配置样式
-      style: {
-        fill: "#FFFFFF",
-        opacity: 0.6,
-      },
-    },
-    xAxis: {
-      label: {
-        autoHide: true,
-        autoRotate: false,
-      },
-    },
-    meta: {
-      type: {
-        alias: "类别",
-      },
-      sales: {
-        alias: "销售额",
-      },
+    xField: "thang",
+    yField: "value",
+    seriesField: "type",
+    isGroup: true,
+    columnStyle: {
+      radius: [20, 20, 0, 0],
     },
   };
+
   return (
     <Card bordered={false}>
       <Column {...config} />
