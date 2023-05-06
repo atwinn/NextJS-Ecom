9eba13669b4f618209f8cef97762cc0424990308
ATwinn <dinhducluong110@gmail.com>
2023-05-06 22:57:13 +0700

    update footer

--- src/component/footer.tsx
+++ src/component/footer.tsx
@@ -1,23 +1,22 @@
 import React from 'react'
 import { Layout } from "antd";
 import Link from 'next/link';
+import { pageRoutes } from '@/redux/constant/page-routes.constant';
 
 const { Footer } = Layout;
 const PageFooter = () => {
     return (
         <Footer className="rounded-lg shadow m-4 bg-zinc-900">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link href="/" className="hover:underline">L3M SHOP</Link>. All Rights Reserved.
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">Bản quyền thuộc về <Link href="/" className="hover:underline">L3M SHOP</Link> @2023.
                 </span>
                 <div className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
 
-                    <Link href="/" className="mr-4 hover:underline md:mr-6 ">About</Link>
+                    <Link href={pageRoutes.contact.route} className="mr-4 hover:underline md:mr-6 ">Liên hệ</Link>
 
-                    <Link href="/" className="mr-4 hover:underline md:mr-6">Privacy Policy</Link>
+                    <Link href={pageRoutes.checkGuarantee.route} className="mr-4 hover:underline md:mr-6">Kiểm tra bảo hành</Link>
 
-                    <Link href="/" className="mr-4 hover:underline md:mr-6">Licensing</Link>
-
-                    <Link href="/" className="hover:underline">Contact</Link>
+                    <Link href={"/"} className="mr-4 hover:underline md:mr-6">Về chúng tôi</Link>
 
                 </div>
             </div>
