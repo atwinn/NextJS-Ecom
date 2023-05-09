afd28a8d5f5c1802deeb9c7ef68262a1536461ad
manhdo1 <dodinhmanh75@gmail.com>
2023-05-09 22:23:51 +0700

    Update home.tsx

--- src/local-page/user/home.tsx
+++ src/local-page/user/home.tsx
@@ -101,21 +101,21 @@ const TrangChu = () => {
         <Carousel autoplay className="mb-4">
           <div>
             <img
-              src="https://images.unsplash.com/photo-1661956601349-f61c959a8fd4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+              src="https://xgear.net/wp-content/uploads/2023/05/Gia-re-zdach.jpg"
               alt="Slide 1"
               className="w-full h-96 object-cover rounded-lg"
             />
           </div>
           <div>
             <img
-              src="https://images.unsplash.com/photo-1681125282872-ecd5e684ecc4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80"
+              src="https://xgear.net/wp-content/uploads/2023/04/Nang-cap-la-phai-gap.jpg"
               alt="Slide 2"
               className="w-full h-96 object-cover rounded-lg"
             />
           </div>
           <div>
             <img
-              src="https://images.unsplash.com/photo-1680695919630-39239c563fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              src="https://xgear.net/wp-content/uploads/2023/04/Helios-Neo-16.jpg"
               alt="Slide 3"
               className="w-full h-96 object-cover rounded-lg"
             />
