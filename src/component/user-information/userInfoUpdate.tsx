f1194ce023124b0860413e2af90484b9aefe90ca
ATwinn <dinhducluong110@gmail.com>
2023-05-11 00:44:59 +0700

    fix date lan 2

--- src/component/user-information/userInfoUpdate.tsx
+++ src/component/user-information/userInfoUpdate.tsx
@@ -104,7 +104,7 @@ const UserUpdateForm = ({ userUpdateData, close, fetch, form }: any) => {
                 <Form.Item label="Ngày Sinh" name="ngaySinh" >
                     <DatePicker placeholder='Chọn ngày' format={dateFormat}
                         // value={dayjs(userUpdateData.attributes.ngaySinh)}
-                        value={dayjs(userUpdateData.attributes.ngaySinh) ? dayjs(userUpdateData.attributes.ngaySinh) : dayjs('2000/01/01', dateFormat)}
+                        value={dayjs(userUpdateData.attributes.ngaySinh) ? dayjs(userUpdateData.attributes.ngaySinh) : dayjs('2000/01/01')}
                     />
                 </Form.Item>
 
